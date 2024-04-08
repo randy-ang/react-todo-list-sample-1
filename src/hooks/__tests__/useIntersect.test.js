f402851d76414ee1d82de18a2339e897674e9fd9
randy-ang <randy.anggadinata@gmail.com>
2024-04-08 21:59:46 +0900

    added unit test for useIntersect

---
+++ src/hooks/__tests__/useIntersect.test.js
@@ -0,0 +1,37 @@
+import { render } from '@testing-library/react'
+import { useRef } from 'react'
+import useIntersect from '../useIntersect'
+
+// Mock IntersectionObserver constructor
+global.IntersectionObserver = jest.fn()
+const mockIntersectionObserver = {
+  observe: jest.fn(),
+  disconnect: jest.fn(),
+}
+
+describe('useIntersect', () => {
+  it('detects intersection with viewport', async () => {
+    global.IntersectionObserver.mockImplementation(function (callback) {
+      callback([{ isIntersecting: true }])
+
+      return mockIntersectionObserver
+    })
+
+    const TestComponent = () => {
+      const ref = useRef(null)
+      const isIntersecting = useIntersect(ref)
+
+      return (
+        <div ref={ref} style={{ height: '1000px' }}>
+          {isIntersecting ? 'Intersecting' : 'Not Intersecting'}
+        </div>
+      )
+    }
+
+    const { findByText } = render(<TestComponent />)
+    const intersectingText = await findByText('Intersecting')
+
+    expect(intersectingText).toBeInTheDocument()
+    expect(mockIntersectionObserver.observe).toHaveBeenCalled()
+  })
+})
